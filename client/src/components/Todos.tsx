21d7314b5ef8e35c056858c3d5e6b47a0bb72b80
Adedolapo Olutuyo <85107972+tuyojr@users.noreply.github.com>
2022-07-16 16:41:28 +0100

    Update Todos.tsx

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -96,8 +96,8 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
         todos,
         loadingTodos: false
       })
-    } catch (e) {
-      alert(`Failed to fetch todos: ${e}`)
+    } catch (e: any) {
+      alert(`Failed to fetch todos: ${e.message}`)
     }
   }
 
